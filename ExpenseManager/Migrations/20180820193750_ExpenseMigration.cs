1eeb914a01e992f4de74ea7839e938eebdd8521c
Adrian Homone <adrianhomone@gmail.com>
2018-08-22 22:38:49 +0300

    Add project files.

---
+++ ExpenseManager/Migrations/20180820193750_ExpenseMigration.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ExpenseManager.Migrations
+{
+    public partial class ExpenseMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ExpenseReports",
+                columns: table => new
+                {
+                    ItemId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    ItemName = table.Column<string>(nullable: false),
+                    Amount = table.Column<decimal>(type: "decimal(10,2)", nullable: false),
+                    Date = table.Column<DateTime>(nullable: false),
+                    Description = table.Column<string>(maxLength: 100, nullable: true),
+                    Category = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ExpenseReports", x => x.ItemId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ExpenseReports");
+        }
+    }
+}
---
+++ ExpenseManager/Models/ExpenseReport.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ExpenseManager.Models
+{
+    public class ExpenseReport
+    {
+        [Key]
+        public int ItemId { get; set; }
+        [Required]
+        public string ItemName { get; set; }
+        [Required]
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(10,2)")]
+        public decimal Amount { get; set; }
+        [DataType(DataType.DateTime)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy", ApplyFormatInEditMode = true)]
+        public DateTime Date { get; set; }
+        [MaxLength(100)]
+        public string Description { get; set; }
+        [Required]
+        public string Category { get; set; }
+    }
+
+    public class ExpenseDBContext : DbContext
+    {
+        public virtual DbSet<ExpenseReport> ExpenseReports { get; set; }
+        public ExpenseDBContext(DbContextOptions options) : base(options) { }
+    }
+}
